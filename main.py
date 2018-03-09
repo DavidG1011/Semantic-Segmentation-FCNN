18ab43b473612083c1007ecad8ec969aaadbfc68
David Gay <djg9610@gmail.com>
2018-03-08 23:36:51 -0500

    Tweaks.
    + Added a kernel initializer. 
    + Small tweaks to network.

--- main.py
+++ main.py
@@ -57,28 +57,34 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
     # TODO: Implement function
     layer7_conv = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, (1,1), padding='same',
-                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01))
 
     layer7_transpose = tf.layers.conv2d_transpose(layer7_conv, num_classes, 4, (2,2), padding='same', 
-                                               kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                               kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3),
+                                               kernel_initializer= tf.random_normal_initializer(stddev=0.01))
 
     layer4_conv = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, (1,1), padding='same',
-                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01))
 
     skip1 = tf.add(layer7_transpose, layer4_conv)
 
 
     layer3_conv = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, (1,1), padding='same',
-                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01))
 
     skip1_transpose = tf.layers.conv2d_transpose(skip1, num_classes, 4, (2,2), padding='same', 
-                                              kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                              kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                              kernel_initializer= tf.random_normal_initializer(stddev=0.01))
 
     skip2 = tf.add(layer3_conv, skip1_transpose)
 
 
     final = tf.layers.conv2d_transpose(skip2, num_classes, 16, (8,8), padding='same', 
-                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
+                                       kernel_initializer= tf.random_normal_initializer(stddev=0.01))
 
     return final
 
@@ -136,7 +142,7 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
                                feed_dict={input_image: image,
                                           correct_label: label,
                                           keep_prob: 0.5,
-                                          learning_rate: 0.0001
+                                          learning_rate: 0.001
                                          })
             print (loss)
 
@@ -171,7 +177,7 @@ def run():
         # TODO: Build NN using load_vgg, layers, and optimize function
 
         epochs = 50;
-        batch_size = 5;
+        batch_size = 6;
 
 
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes])
